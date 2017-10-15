6cb311a3eceb7ff52fe0528f6523bb3e033b9149
rpatel372 <reemapatel97@gmail.com>
2017-10-14 19:55:33 -0700

    Added web scraping - need to add data to Mongo

--- webScrapingTest.py
+++ webScrapingTest.py
@@ -2,10 +2,21 @@
 import urllib2 
 from bs4 import BeautifulSoup
 
-wiki = "http://www.counseling.gatech.edu/content/referral-services"
-page = urllib2.urlopen(wiki)
-soup = BeautifulSoup(page)
-soup.prettify()
-letters = soup.find_all("div", class_="body with-aside")
+links = ["http://www.counseling.gatech.edu/content/couples-counseling",
+		"http://www.counseling.gatech.edu/content/individual-counseling",
+		"http://www.counseling.gatech.edu/content/students-crisis",
+		"http://www.counseling.gatech.edu/content/referral-services",
+		"http://www.counseling.gatech.edu/content/sexual-assault-response",
+		"http://www.counseling.gatech.edu/content/life-skills-workshops",
+		"http://www.counseling.gatech.edu/content/guide-psychoeducational-assessment",
+		"http://www.counseling.gatech.edu/content/career-counseling",
+		"http://www.counseling.gatech.edu/content/psychiatric-services",
+		"http://www.counseling.gatech.edu/content/group-counseling"]
 
-print(letters)
+for link in links:
+	page = urllib2.urlopen(wiki)
+	soup = BeautifulSoup(page)
+	soup.prettify()
+	bodyText = soup.find_all("div", class_="body with-aside")
+
+	# ADD TO DATABASE HERE
